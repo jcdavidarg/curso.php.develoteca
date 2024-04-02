faebd51b6a86e2e94498dc583342064f761a129c
jcdavidarg <jcdavidarg@gmail.com>
2024-04-02 20:33:25 -0300

    ejercicio 36 include

---
+++ ejercicio36-include.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sitio web</title>
+</head>
+<body>
+    <?php include "./ejercicio36_1.php";?>
+</body>
+</html>
+
