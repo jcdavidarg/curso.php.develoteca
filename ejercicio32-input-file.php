243d1ac4108580955c880693a5e3f77044e6d9e8
jcdavidarg <jcdavidarg@gmail.com>
2024-04-02 01:35:58 -0300

    ejercicio 32 input file

---
+++ ejercicio32-input-file.php
@@ -0,0 +1,31 @@
+<?php
+
+if($_POST)
+{
+    print_r($_POST);
+    print_r($_FILES["archivo"]["type"]); // SELECCIONO EL INPUT MEDIANTE SU NAME Y ELIGO SEGUN LO QUE TENGA DENTRO
+
+    move_uploaded_file($_FILES["archivo"]["tmp_name"], $_FILES["archivo"]["name"]); //MOVER ARCHIVOS SUBIDOS, TEMPORALES A RUTAS
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="./ejercicio32-input-file.php" enctype="multipart/form-data" method="post"> <!-- PARA MANEJAR ARCHIVOS -->
+    Imagen:
+    <input type="file" name="archivo" id="">
+    <br/>
+    <br/>
+    <input type="submit" name="enviar" value="Enviar informacion">
+
+</form>
+    
+</body>
+</html>
