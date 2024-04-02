a033d2949611590501697f5e0bc8c845087a58fd
jcdavidarg <jcdavidarg@gmail.com>
2024-04-02 20:55:45 -0300

    ejercicio 37 require y once y diferencias con include

--- ejercicio36-include.php
+++ ejercicio36-include.php
@@ -6,7 +6,9 @@
     <title>sitio web</title>
 </head>
 <body>
-    <?php include "./ejercicio36_1.php";?>
+    <?php include_once "./ejercicio36_1.php";?> <!-- PERMITE MOSTRAR SI HAY ERROR Y TIRA UN WARNING -->
+    <?php include_once "./ejercicio36_1.php";?> <!-- ONCE SOLO PERMITE UNA SOLA VEZ INCLUIR ALGO NO SE PUEDE REPETIR ESE ALGO -->
+    <?php echo "Hola, estoy en la pagina principal";?>
 </body>
 </html>
 
