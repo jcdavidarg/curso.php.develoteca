80aefea6c17ccb5e9a0b3497880f0ffcff391617
jcdavidarg <jcdavidarg@gmail.com>
2024-04-02 18:31:39 -0300

    ejercicio 34 encode json

---
+++ ejercicio34-json-encode.php
@@ -0,0 +1,8 @@
+<?php
+
+$personas = array("David" => 33, "Vero" => 36, "Nadia" => 30);
+
+echo json_encode($personas); //LO TRANSFORMA A JSON
+// CONVERTIR ARRAYS EN JSON PARA SERVIR ESA INFORMACION
+
+?>
